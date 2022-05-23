33ac8498fe38a608e12fbeafa6378afd3ccc3fa1
Alexandr <net_box@ukr.net>
2022-05-23 03:01:14 +0300

    feat: javascript

--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,152 @@
 'use strict';
 
-const ELEM = (selector) => document.querySelector(selector);
+const TOP_MENU = document.querySelector('.top__menu');  
+const BOTTOM = document.querySelector('.bottom');
+const BOTTOM_INNER = document.querySelector('.bottom__inner');
+const TOP_FONT_WRAPPER = document.querySelector('.top__font-wrapper');
+const TOP_FONT_LIST = document.querySelector('.top__font-list');
+const TOP_SIZE = document.querySelector('.top__size');
+const TOP_SIZE_LIST = document.querySelector('.top__size-list');
+const TOP_COLOR_WRAPPER = document.querySelector('.top__color-wrapper');
+const TOP_COLOR_TEXT = document.querySelector('.top__color-text');
+const TOP_COLOR_PICKER = document.querySelector('.top__color-picker');
+const TOP_COLOR_TITLE = document.querySelector('.top__color-title');
+const TOP_COLOR_CUB = document.querySelector('.top__color-cub');
+const TOP_COLOR_BG = document.querySelector('.top__color-bg');
+const TOP_COLOR_LIST = document.querySelector('.top__color-list');
+const TOP_COLOR_ITEM = document.querySelector('.top__color-item');
+const TOP_COLOR_ITEM_ACTIVE = document.querySelector('.top__color--active');
+const MODAL_CLOSE = document.querySelector('.modal-close');
+const TAB = document.querySelector('.tab');
+const TOP_IMAGES = document.querySelector('.top__images');
 
-ELEM('.middle__edit-btn').addEventListener('click', () =>{
-    ELEM('.bottom__textarea').value = ELEM('.top').innerHTML;
-    ELEM('.bottom__edit').classList.add('show');
-    ELEM('.bottom__style').classList.remove('show');
-});
 
-ELEM('.bottom__save-btn').addEventListener('click', () =>{
-    ELEM('.top').innerHTML = ELEM('.bottom__textarea').value;
-    ELEM('.bottom__edit').classList.remove('show');
-});
+let isColor = false;
 
-ELEM('.middle__style-btn').addEventListener('click', () =>{
-  ELEM('.bottom__edit').classList.remove('show');
-  ELEM('.bottom__style').classList.add('show');
-});
-
-//radio-btn
-ELEM('.bottom__size').addEventListener('click', (event) =>{
-  if(event.target.name === 'fontSize'){
-    ELEM('.top').style.fontSize = event.target.value + 'px';
+TOP_MENU.addEventListener('click', (event) => {
+  if(event.target.name === 'bold' && BOTTOM_INNER.style.fontWeight === 'bold'){
+    BOTTOM_INNER.style.fontWeight = 'inherit';
+  }
+  else if(event.target.name === 'bold'){
+    BOTTOM_INNER.style.fontWeight = 'bold';
+  }
+  else if(event.target.name === 'italic' && BOTTOM_INNER.style.fontStyle === 'italic'){
+    BOTTOM_INNER.style.fontStyle = 'inherit';
+  }
+  else if(event.target.name === 'italic'){
+    BOTTOM_INNER.style.fontStyle = 'italic';
+  }
+  else if(event.target.name === 'underline' && BOTTOM_INNER.style.textDecoration === 'underline'){
+    BOTTOM_INNER.style.textDecoration = 'inherit';
+  }
+  else if(event.target.name === 'underline'){
+    BOTTOM_INNER.style.textDecoration = 'underline';
+  }
+  else if(event.target.name === 'strikethrough' && BOTTOM_INNER.style.textDecoration === 'line-through'){
+    BOTTOM_INNER.style.textDecoration = 'inherit';
+  }
+  else if(event.target.name === 'strikethrough'){
+    BOTTOM_INNER.style.textDecoration = 'line-through';
+  }
+  else if(event.target.name === 'left' && BOTTOM_INNER.style.textAlign === 'left'){
+    BOTTOM_INNER.style.textAlign = 'inherit';
+  }
+  else if(event.target.name === 'left'){
+    BOTTOM_INNER.style.textAlign = 'left';
+  }
+  else if(event.target.name === 'center' && BOTTOM_INNER.style.textAlign === 'center'){
+    BOTTOM_INNER.style.textAlign = 'inherit';
+  }
+  else if(event.target.name === 'center'){
+    BOTTOM_INNER.style.textAlign = 'center';
+  }
+  else if(event.target.name === 'right' && BOTTOM_INNER.style.textAlign === 'right'){
+    BOTTOM_INNER.style.textAlign = 'inherit';
+  }
+  else if(event.target.name === 'right'){
+    BOTTOM_INNER.style.textAlign = 'right';
+  }
+  else if(event.target.name === 'font'){
+    TOP_SIZE.classList.remove('block');
+    TOP_FONT_WRAPPER.classList.add('block');
+  }
+  else if(event.target.name === 'font-size'){
+    TOP_FONT_WRAPPER.classList.remove('block');
+    TOP_SIZE.classList.add('block');
+  }
+  else if(event.target.name === 'palette'){
+    TOP_COLOR_WRAPPER.classList.add('block');
+    TOP_COLOR_TEXT.classList.add('block');
+    TOP_COLOR_PICKER.classList.remove('hide');
+    isColor = true;
+  }
+  else if(event.target.name === 'image'){
+    isColor = false;
+    TOP_COLOR_WRAPPER.classList.add('block');
+    TOP_COLOR_BG.classList.add('block');
+    TOP_COLOR_PICKER.classList.remove('hide');
+    TOP_COLOR_LIST.classList.remove('hide');
   }
 });
 
-//select
-ELEM('.bottom__family').addEventListener('change', (event) => {
-  ELEM('.top').style.fontFamily = event.target.value;
+TOP_FONT_WRAPPER.addEventListener('click', (event) => {
+  BOTTOM_INNER.style.fontFamily = event.target.dataset.name;
+  TOP_FONT_WRAPPER.classList.remove('block');
+});
+TOP_FONT_LIST.addEventListener('mouseover', ( event ) => {
+  BOTTOM_INNER.style.fontFamily = event.target.dataset.name;
 });
 
-//colorPicker
-ELEM('.bottom__colors').addEventListener('click', (event) => {
-  ELEM('.bottom__style-right').classList.add('flex');
-  console.dir(event.target.name);
-  ELEM('.cube').addEventListener('click', e => {
-    if(event.target.name == 'bg'){
-      ELEM('.top').style.backgroundColor = e.target.name;
-    } 
-    else if(event.target.name == 'color'){
-      ELEM('.top').style.color = e.target.name;
+TOP_SIZE.addEventListener('click', (event) => {
+  BOTTOM_INNER.style.fontSize = event.target.dataset.name + 'px';
+  TOP_SIZE.classList.remove('block');
+});
+TOP_SIZE.addEventListener('mouseover', ( event ) => {
+  BOTTOM_INNER.style.fontSize = event.target.dataset.name + 'px';
+});
+
+MODAL_CLOSE.addEventListener('click', () => {
+  TOP_COLOR_WRAPPER.classList.remove('block');
+});
+
+TOP_COLOR_PICKER.addEventListener('click', (event) => {
+  if(isColor){
+    BOTTOM_INNER.style.color = getComputedStyle(event.target).backgroundColor;
+    TOP_COLOR_WRAPPER.classList.remove('block');
+  } else {
+    BOTTOM.style.backgroundColor = getComputedStyle(event.target).backgroundColor;
+    TOP_COLOR_WRAPPER.classList.remove('block');
+  }
+  isColor = false;
+});
+
+window.onload = function () {
+  TOP_COLOR_LIST.addEventListener('click', tabs);
+  function tabs(event) {
+    if (event.target.className == 'top__color-item'){
+      //dataTab - номер вкладки, которую нужно отобразить
+      let dataTab = event.target.getAttribute('data-tab');
+      //отключаю класс active
+      let tabH = document.getElementsByClassName('top__color-item');
+      for(let i = 0; i < tabH.length; i++){
+        tabH[i].classList.remove('top__color--active');
+      }
+      event.target.classList.add('top__color--active');
+      //все вкладки с содержимым
+      let tabBody = document.getElementsByClassName('tab');
+      for (let i = 0; i < tabBody.length; i++){
+        if (dataTab == i) {
+          tabBody[i].classList.remove('hide');
+        }else {
+          tabBody[i].classList.add('hide');
+        }
+      }
     }
-    console.log(e.target.name);
-  });
+  }
+};
+
+TOP_IMAGES.addEventListener('click', (event) => {
+  console.dir(event.target);
+  console.log(event.target.style.url);
+  BOTTOM.style.backgroundImage = event.target.srcElement;
 });
-
-
-
-
